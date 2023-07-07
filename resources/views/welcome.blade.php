7628fc091aa5025423714bd03c1601ef0d41d285
Leodisey125 <119302416+Leodisey125@users.noreply.github.com>
2023-07-06 20:39:51 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,19 +9,6 @@
             <img src="/img/logo_portada.png" class="img-fluid" >
         </a>
     </div>
-</nav>
-<!-- Logo portada fin-->
-
-<!-- Boton portada -->
-<nav class="navbar navbar-light bg-light2">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="{{route('pantalla.opciones_registro')}}">
-            <div class="d-grid gap-2 col-6 mx-auto">
-                <button class="btn btn-primary" type="button">Iniciar</button>
-            </div>
-        </a>
-    </div>
-</nav>
 
 <!-- Boton portada fin s-->
 @endsection
