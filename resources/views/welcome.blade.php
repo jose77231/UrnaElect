3fd7ad77682970081189f0ff51d112d0f3b19d73
Leodisey125 <119302416+Leodisey125@users.noreply.github.com>
2023-07-06 20:40:10 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,1 @@
-@extends('layout/plantilla')
-@section('tituloPagina', 'UrnaElect')
 
-@section('contenido')
-<!-- Logo portada -->
-<nav class="navbar navbar-light bg-light2">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-            <img src="/img/logo_portada.png" class="img-fluid" >
-        </a>
-    </div>
-
-<!-- Boton portada fin s-->
-@endsection
